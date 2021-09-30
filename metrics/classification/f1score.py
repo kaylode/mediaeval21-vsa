33a2d6ab581c6e2dd8ed75846759249013778b23
Kaylode <kayp.kieran@gmail.com>
2021-09-30 21:42:35 +0700

    fix f1

--- metrics/classification/f1score.py
+++ metrics/classification/f1score.py
@@ -2,13 +2,33 @@ import torch
 import numpy as np
 from sklearn.metrics import f1_score
 
+def postprocess_multilabel(tensor, threshold):
+    tensor = torch.sigmoid(torch.from_numpy(tensor))
+    tensor = tensor.numpy()
+    tensor = tensor > threshold
+    outputs = tensor.astype(np.int)
+    return outputs
+
+def postprocess_multiclass(tensor):
+    tensor = torch.argmax(torch.from_numpy(tensor),dim=1)
+    tensor = tensor.numpy()
+    outputs = tensor.astype(np.int)
+    return outputs
+
+def get_postprocess_fn(types):
+    if types == 'multiclass':
+        return postprocess_multiclass
+    if types == 'multilabel':
+        return postprocess_multilabel
+
 class F1ScoreMetric():
     """
     F1 Score Metric (including macro, micro)
     """
-    def __init__(self, multilabel_threshold=0.5, average = 'macro'):
+    def __init__(self, types="multiclass", average = 'macro'):
         self.average = average
-        self.multilabel_threshold = multilabel_threshold
+        self.types = types
+        self.postprocess_fn = get_postprocess_fn(types)
         self.reset()
 
     def update(self, outputs, targets):
@@ -21,10 +41,8 @@ class F1ScoreMetric():
     def compute(self): 
         self.targets = np.concatenate(self.targets)
         self.preds = np.concatenate(self.preds)
-        self.preds = torch.sigmoid(torch.from_numpy(self.preds))
-        self.preds = self.preds.numpy()
-        self.preds = self.preds > self.multilabel_threshold
-        self.preds = self.preds.astype(np.int)
+        
+        self.preds = self.postprocess_fn(self.preds)
         return f1_score(self.targets, self.preds, average=self.average)
 
     def value(self):
