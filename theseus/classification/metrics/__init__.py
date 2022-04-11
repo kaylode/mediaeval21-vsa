e77cf018c13660dd216399a9ef68c0809878affc
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-04-11 14:57:30 +0700

    fix bugs

--- theseus/classification/metrics/__init__.py
+++ theseus/classification/metrics/__init__.py
@@ -1,14 +1,12 @@
 from theseus.base.metrics import METRIC_REGISTRY
 
 from .accuracy import *
-from .bl_accuracy import *
 from .f1 import *
 from .confusion_matrix import *
 from .errorcases import *
 from .projection import *
 
 METRIC_REGISTRY.register(Accuracy)
-METRIC_REGISTRY.register(BalancedAccuracyMetric)
 METRIC_REGISTRY.register(F1ScoreMetric)
 METRIC_REGISTRY.register(ConfusionMatrix)
 METRIC_REGISTRY.register(ErrorCases)
@@ -16,10 +14,8 @@ METRIC_REGISTRY.register(EmbeddingProjection)
 
 from .multilabel.accuracy import MultilabelAccuracy
 from .multilabel.f1 import MultilabelF1ScoreMetric
-from .multilabel.bl_accuracy import MultilabelBalancedAccuracy
 from .multilabel.confusion_matrix import MultilabelConfusionMatrix
 
 METRIC_REGISTRY.register(MultilabelAccuracy)
-METRIC_REGISTRY.register(MultilabelBalancedAccuracy)
 METRIC_REGISTRY.register(MultilabelF1ScoreMetric)
 METRIC_REGISTRY.register(MultilabelConfusionMatrix)
--- theseus/classification/utilities/logits.py
+++ theseus/classification/utilities/logits.py
@@ -2,17 +2,17 @@ import numpy as np
 import torch
 from theseus.utilities.cuda import move_to, detach
 
-def multiclass_logits2labels(outputs, return_probs: bool = False):
-    probs, outputs = torch.max(torch.softmax(outputs),dim=1)
+def multilabel_logits2labels(outputs, return_probs: bool = False):
+    probs, outputs = torch.max(torch.softmax(outputs, dim=-1),dim=-1)
     
     probs = move_to(detach(probs), torch.device('cpu'))
     outputs = move_to(detach(outputs), torch.device('cpu'))
 
     if return_probs:
-        return outputs, probs
+        return outputs.long(), probs
     return outputs
 
-def multilabel_logits2labels(outputs, threshold=0.5, return_probs: bool = False):
+def multiclass_logits2labels(outputs, threshold=0.5, return_probs: bool = False):
     assert threshold is not None, "Please specify threshold value for sigmoid"
     probs = torch.sigmoid(outputs) 
     outputs = outputs > threshold
@@ -21,7 +21,7 @@ def multilabel_logits2labels(outputs, threshold=0.5, return_probs: bool = False)
     outputs = move_to(detach(outputs), torch.device('cpu'))
 
     if return_probs:
-        return outputs, probs
+        return outputs.long(), probs
     return outputs
 
 def logits2labels(outputs, type='multiclass', threshold: float = None, return_probs: bool = False):
